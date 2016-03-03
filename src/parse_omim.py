95f8cf8d74be5a110bb6b0cc0e448a0584c7097c
Daniel Birnbaum <daniel@MGH002911.local>
2016-03-02 22:19:15 -0500

    Regenerated OMIM files with more transparent gene name translations

--- src/parse_omim.py
+++ src/parse_omim.py
@@ -14,30 +14,13 @@ from collections import defaultdict
 from gene_list_utils import *
 
 
-def simplify_gene_list(genes, thesaurus):
-	genes = re.sub(' ', '', genes).split(',')
-	new = set()
-	for gene in genes:
-		if gene in thesaurus:
-			synonyms = thesaurus[gene]
-			if len(synonyms) > 1:
-				# if a certain symbol maps to multiple "approved" synonyms, pick the first one different from the symbol itself
-				syn = filter(lambda x: x != gene, synonyms)[0]
-				new.add(syn)
-			else:
-				new.add(synonyms[0])
-		else:
-			new.add(gene)
-
-	return ','.join(new)
-
-"""
-Returns mapping from gene symbol to list of synonymous, approved gene symbols 
-ideally, there should only be one approved synonym for any given symbol, but the file has lines where
-a non-approved symbol will appear in the approved column and thus will be mapped to itself
-e.g. FRAXA NA
-"""
 def get_gene_thesaurus(filename):
+	"""
+	Returns mapping from gene symbol to list of synonymous, approved gene symbols 
+	ideally, there should only be one approved synonym for any given symbol, but the file has lines where
+	a non-approved symbol will appear in the approved column and thus will be mapped to itself
+	e.g. FRAXA NA
+	"""
 	thesaurus = defaultdict(list)
 	with open(filename, 'r') as lines:
 		for line in lines:
@@ -70,12 +53,12 @@ def main(args):
 	else:
 		o = open(args.output, 'w')
 
-	header = ['phenotype', 'phenotypeInheritance', 'phenotypeMimNumber', 'chromosome', 'gene', 'geneMimNumber', 'comments']
+	header = ['genes', 'hgnc_synonyms', 'hgnc_genes', 'phenotype', 'phenotypeInheritance', 
+			  'geneMimNumber','phenotypeMimNumber', 'chromosome', 'comments']
 	o.write('\t'.join(header) + '\n')
 	header = dict(zip(header, range(len(header))))
 
-	if args.simplify_genes:
-		t = get_gene_thesaurus(args.hgnc)
+	t = get_gene_thesaurus(args.hgnc)
 	sys.stdout.write("\rOn chromosome %s .." % request_data['chromosome'])
 	sys.stdout.flush()
 	while True:
@@ -106,14 +89,23 @@ def main(args):
 
 		# write response data
 		for g in geneMapList:
-			geneMap = g['geneMap']
-			
 			line = ['']*len(header.keys())
-			if args.simplify_genes:
-				genes = simplify_gene_list(geneMap['geneSymbols'], t)
+			geneMap = g['geneMap']
+			genes = re.sub(' ', '', geneMap['geneSymbols']).split(',')
+			hgnc_genes = [','.join(t[gene]) if t[gene] else 'NA' for gene in genes]
+			if args.use and all(map(lambda g:g == 'NA', hgnc_genes)):
+				continue
+
+			line[header['hgnc_synonyms']] = '|'.join(hgnc_genes)
+			tmp = reduce(lambda a, b: a + ',' + b, hgnc_genes)
+			hgnc_genes = set(filter(lambda x:x != 'NA', tmp.split(',')))
+			if not hgnc_genes: 
+				hgnc_genes = 'NA'
 			else:
-				genes = re.sub(' ', '', geneMap['geneSymbols'])
-			line[header['gene']] = genes
+				hgnc_genes = ','.join(hgnc_genes)
+
+			line[header['genes']] = '|'.join(genes)
+			line[header['hgnc_genes']] = hgnc_genes
 			line[header['chromosome']] = geneMap['chromosomeSymbol']
 			line[header['geneMimNumber']] = geneMap['mimNumber']
 			
@@ -138,6 +130,8 @@ def main(args):
 
 					line = map(str, line)
 					o.write('\t'.join(line) + '\n')
+			elif args.use:
+				continue
 			else:
 				line[header['phenotype']] = 'NA'
 				line[header['phenotypeInheritance']] = 'NA'
@@ -152,14 +146,17 @@ def main(args):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--simplify_genes', action='store_true')
-	parser.add_argument('--hgnc', dest='hgnc', help='Path to gene thesaurus file.')
-	parser.add_argument('--output', dest='output', default=sys.stdout)
+	parser.add_argument('--hgnc', dest='hgnc', help='Path to gene thesaurus file.', required=True)
+	parser.add_argument('-o', '--output', dest='output', default=sys.stdout)
 	parser.add_argument('--chrom', help='Only get data for given chromosome.')
+	parser.add_argument('--use', action='store_true', help='Only output entries with a gene AND '
+														   'associated phenotype AND WHERE '
+														   'the gene can be matched to an HGNC-approved symbol.')
 	args = parser.parse_args()
-	if args.simplify_genes and not args.hgnc:
-		parser.error('If --simplify_genes is set, then you need to provide a gene thesaurus via --hgnc option.')
 
 	main(args)
+
+# e.g python parse_omim.py --hgnc gene_symbol_thesaurus.txt --simplify_genes --output omim_table.txt
+
 
 
